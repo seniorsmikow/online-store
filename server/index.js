1baeb4d935eb9d4ab4fe0e97ea0cfe7b1d0d4a1e
Ilya <senior.smikow@yandex.ru>
2021-03-10 22:55:38 +0300

    Server development part 3

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const sequelize = require('./db');
+const models = require('./models/models');
 
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
