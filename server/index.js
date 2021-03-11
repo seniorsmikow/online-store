be8fffde3d5028f15bdf4e4a1a11f26750c5fedf
Ilya <senior.smikow@yandex.ru>
2021-03-11 20:58:28 +0300

    Server development part 5

--- server/index.js
+++ server/index.js
@@ -3,12 +3,17 @@ const express = require('express');
 const sequelize = require('./db');
 const models = require('./models/models');
 const cors = require('cors');
+const router = require('./routes/index');
 
 
 const PORT = process.env.PORT || 5420;
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+app.use('/api', router);
+
+
 
 const start = async () => {
     try {
