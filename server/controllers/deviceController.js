63a5786d0ac2ceade9640f5674f6d24f77c77a6a
Ilya <senior.smikow@yandex.ru>
2021-03-17 21:06:01 +0300

    Finished controllers

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,6 +1,6 @@
 const uuid = require('uuid'); // генерирует рандомные неповторяющиеся ID 
 const path = require('path');
-const {Device} = require('../models/models');
+const {Device, DeviceInfo} = require('../models/models');
 const ApiError = require('../error/apiError');
 
 class DeviceController {
@@ -14,23 +14,65 @@ class DeviceController {
 
             const device = await Device.create({name, price, brandId, typeId, img: fileName}); // rating не передаём, по умолчанию 0
 
+            if(info) {
+                info = JSON.parse(info);
+                info.forEach(i => 
+                        DeviceInfo.create({
+                            title: i.title,
+                            description: i.description,
+                            deviceId: device.id
+                        })
+                    );
+            }
+
             // при отправке запроса тип form-data! т.к. нужно прикрепить img
 
             return res.json(device);
-        } catch(e) {
 
+        } catch(e) {
             next(ApiError.badRequest(e.message));
         }
-
-        
     } 
 
     async getAllDevices(req, res) {
-        
+
+        let {brandId, typeId, limit, page} = req.query; // limit & page для пагинации 
+        page = page || 1;
+        limit = limit || 8;
+        let offset = page * limit - limit;
+
+        let devices;
+
+        if(!brandId && !typeId) {
+            //devices = await Device.findAll({limit, offset}); 
+            // для работы на Front части приложения лучше использовать функцию findAndCountAll. Она создана специально для пагинации
+            devices = await Device.findAndCountAll({limit, offset});
+        }
+
+        if(brandId && !typeId) {
+            devices = await Device.findAndCountAll({where: {brandId}, limit, offset});
+        }
+
+        if(!brandId && typeId) {
+            devices = await Device.findAndCountAll({where: {typeId}, limit, offset});
+        }
+
+        if(brandId && typeId) {
+            devices = await Device.findAndCountAll({where: {brandId, typeId}, limit, offset});
+        }
+
+        return res.json(devices);
     }
 
     async getOneDevice(req, res) {
-        
+
+        const {id} = req.params; // параметр указывали в router
+        const device = await Device.findOne({
+            where: {id},
+            include: [{model: DeviceInfo, as: 'info'}]
+        });
+
+        return res.json(device);
     }
 }
 
--- server/models/models.js
+++ server/models/models.js
@@ -72,7 +72,7 @@ Rating.belongsTo(Device);
 Device.hasMany(BasketDevice);
 BasketDevice.belongsTo(Device);
 
-Device.hasMany(DeviceInfo);
+Device.hasMany(DeviceInfo, {as: 'info'}); // это info появляется в DeviceController / createDevice
 DeviceInfo.belongsTo(Device);
 
 Type.belongsToMany(Brand, {through: TypeBrand}); // при связи многие ко многим нужен второй параметр из спец типа 
